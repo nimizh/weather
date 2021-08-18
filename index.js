59127fd20af91ed8f5c8769dfb47a36d2f051f38
Nimish Wadhawan <86304541+nimizh@users.noreply.github.com>
2021-08-18 10:51:15 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,43 @@
+let weather = {
+  apikeys: "e4f587abf3de74b2c0e43f94f4880666",
+  fetchweather: function (city) {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apikeys}`
+    )
+      .then((respone) => respone.json())
+      .then((data) => this.displayweather(data));
+  },
+  displayweather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+
+    document.querySelector(".city").innerText = `Weather in ${name}`;
+    document.querySelector(
+      ".icon"
+    ).src = `http://openweathermap.org/img/wn/${icon}.png`;
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = `${temp}°C`;
+    document.querySelector(".humidity").innerText = `Humidity: ${humidity}%`;
+    document.querySelector(".wind").innerText = `Wind Speed: ${speed}km/h`;
+    document.querySelector(".weather").classList.remove("loading");
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name + "')";
+  },
+  search: function () {
+    this.fetchweather(document.querySelector(".search-bar").value);
+  },
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
+weather.fetchweather("New Delhi");
